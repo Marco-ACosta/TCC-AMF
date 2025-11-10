7989b9a257549d18b94be47fb33bf0490fccd52d
Marco <marco.a.lmc.16@gmail.com>
2025-11-10 19:23:59 -0300

    removing unused repos

--- tradutor/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-        NEXT_PUBLIC_APP_ENV: process.env.REACT_APP_ENV,
-    }
-};
-
-export default nextConfig;
